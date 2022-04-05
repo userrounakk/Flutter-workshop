f287a7a065c7a03fd208ab3187c696ef5b3a177d
Rounak Agrawal <rounak1430@gmail.com>
2022-04-05 17:14:46 +0545

    ListView

--- listview/lib/Pages/homepage.dart
+++ listview/lib/Pages/homepage.dart
@@ -3,22 +3,27 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   var toDoItems = [
     {
+      'id': 1,
       'title': 'Task One',
       'description': 'This is Task One',
     },
     {
+      'id': 2,
       'title': 'Task Two',
       'description': 'This is Task Two',
     },
     {
+      'id': 3,
       'title': 'Task Three',
       'description': 'This is Task Three',
     },
     {
+      'id': 4,
       'title': 'Task Four',
       'description': 'This is Task Four',
     },
     {
+      'id': 5,
       'title': 'Task Five',
       'description': 'This is Task Five',
     },
@@ -36,7 +41,9 @@ class HomePage extends StatelessWidget {
             itemCount: toDoItems.length,
             itemBuilder: (BuildContext context, index) {
               return ListTile(
-                leading: const Icon(Icons.add_circle),
+                leading: CircleAvatar(
+                  child: Text(toDoItems[index]['title'].toString()),
+                ),
                 title: Text(toDoItems[index]['title'].toString()),
                 subtitle: Text(toDoItems[index]['description'].toString()),
               );
